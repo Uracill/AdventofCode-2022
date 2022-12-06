eb350a6591311298e3ca792625f0c3a3b0873434
Daniel Flat <daniel.flat@wps.de>
2022-12-06 10:00:19 +0100

    Tag 6

--- src/main/kotlin/Tag6/Tag6_1.kt
+++ src/main/kotlin/Tag6/Tag6_1.kt
@@ -3,5 +3,35 @@ package Tag6
 import java.io.File
 
 fun main(args: Array<String>): Unit {
-    val input = File("src/main/kotlin/Tag6/Tag6_input.txt").readLines()
+    val input = File("src/main/kotlin/Tag6/Tag6_input.txt").readText()
+    var result = 0
+    val numberOfDifferentChars = 4
+    for (i in numberOfDifferentChars until input.length) {
+        if(result == 0)
+            for(j in i - numberOfDifferentChars until i) {
+                if(areAllDifferent(input.substring(j, i)))
+                    result = i
+                    break
+            }
+        else {
+            break
+        }
+    }
+    println(result) //1953
+}
+
+private fun areAllDifferent(substring: String): Boolean {
+    var different = true
+    for(i in 0 until substring.length) {
+        if(different) {
+            for(j in i + 1 until substring.length) {
+                if(substring.get(i) == substring.get(j))
+                    different = false
+            }
+        }
+        else {
+            break
+        }
+    }
+    return different
 }
