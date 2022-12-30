020857e01a68ce2bdb53a35f4718c8938edb2e5f
0flat <daniel.flat@studium.uni-hamburg.de>
2022-12-30 13:29:08 +0100

    Tag 10

--- src/main/kotlin/Tag10/Tag10_1.kt
+++ src/main/kotlin/Tag10/Tag10_1.kt
@@ -4,4 +4,29 @@ import java.io.File
 
 fun main(args: Array<String>): Unit {
     val input = File("src/main/kotlin/Tag10/Tag10_input.txt").readLines()
+    var result = 0
+    var signalList = mutableListOf<Int>()
+    var x = 1
+    var clock = 1
+    for(command in input) {
+        if(command.equals("noop")) {
+            clock++
+        }
+        else {
+            val number = command.split(" ")[1].toInt()
+            clock++
+            if(clock == 20 || clock == 60 || clock == 100
+                || clock == 140 || clock == 180 || clock == 220) {
+                signalList.add(clock * x)
+            }
+            clock++
+            x += number
+        }
+        if(clock == 20 || clock == 60 || clock == 100
+            || clock == 140 || clock == 180 || clock == 220) {
+            signalList.add(clock * x)
+        }
+    }
+    result = signalList.sum()
+    println(result) //12460
 }
--- src/main/kotlin/Tag10/Tag10_2.kt
+++ src/main/kotlin/Tag10/Tag10_2.kt
@@ -4,4 +4,38 @@ import java.io.File
 
 fun main(args: Array<String>): Unit {
     val input = File("src/main/kotlin/Tag10/Tag10_input.txt").readLines()
+    var result = 0
+    var signalList = Array(6) { CharArray(40) }
+    var x = 1
+    var clock = 1
+    for(command in input) {
+        if(command.equals("noop")) {
+            drawSignal(signalList, clock, x)
+            clock++
+        }
+        else {
+            val number = command.split(" ")[1].toInt()
+            drawSignal(signalList, clock, x)
+            clock++
+            drawSignal(signalList, clock, x)
+            x += number
+            clock++
+        }
+    }
+    for(col in 0 until 6) {
+        for(row in 0 until 40) {
+            print(signalList[col][row])
+        }
+        println()
+    }
+    //EZFPRAKL
+}
+
+fun drawSignal(signalList: Array<CharArray>, clock: Int, x: Int) {
+    if((clock - 1) % 40 == x - 1 || (clock - 1) % 40 == x || (clock - 1) % 40 == x + 1) {
+        signalList[(clock - 1) / 40][(clock - 1) % 40] = '#'
+    }
+    else {
+        signalList[(clock - 1) / 40][(clock - 1) % 40] = '.'
+    }
 }
